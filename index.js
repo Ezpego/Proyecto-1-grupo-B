fd54315c6326222751497f0de48bb350c98e390a
Ezequiel Pérez <ezpego@gmail.com>
2023-10-28 14:06:22 +0200

    JS modificado y añadido JSON

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+function tripleXXX() {
+    
+    fetch("/quiz.json")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+}
+
+tripleXXX();
