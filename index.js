53e0e8b85eab29764ff43e0e17b1dca3cea34ec6
Ezequiel Pérez <ezpego@gmail.com>
2023-10-29 15:14:31 +0100

    algo de luz

--- index.js
+++ index.js
@@ -11,50 +11,100 @@
             .then(data => {
                 preguntas = data;
                 mostrarPregunta();
+            })
+            .catch(error => {
+                console.error(error);
             });
+
 
         function mostrarPregunta() {
             const pregunta = preguntas[preguntaActual];
-            const preguntaHTML = `
-                <h2>${pregunta.question}</h2>
-                <ul>
-                    ${pregunta.answers.map(respuesta => `<li>${respuesta}</li>`).join('')}
-                </ul>
+            const estructuraHTML = `
+                <form>
+                    <fieldset>
+                    <legend> Juego de Preguntas y Respuestas </legend>
+                    <h2>${pregunta.question}</h2>
+
+                    <input type="radio" id="uno" name="options"/>
+                    <label for="uno" >${pregunta.answers[0]}</label>
+
+                    <input type="radio" id="dos" name="options"/>
+                    <label for="dos" >${pregunta.answers[1]}</label>
+
+                    <input type="radio" id="tres" name="options"/>
+                    <label for="tres" >${pregunta.answers[2]}</label>
+
+                    <input type="radio" id="cuatro" name="options"/>
+                    <label for="cuatro" >${pregunta.answers[3]}</label>
+
+                    <button onclick="verificarRespuestaYAvanzar()">Comprobar respuesta</button>
+                    <div id="marcador"> Score : "aciertos" / ${preguntaActual +1}</div>
+                    </fieldset>
+                </form>
             `;
-            preguntaContainer.innerHTML = preguntaHTML;
-        }
-
-        function mostrarSiguientePregunta() {
-            preguntaActual = (preguntaActual + 1) % preguntas.length;
-            mostrarPregunta();
+            preguntaContainer.innerHTML = estructuraHTML;
         }
 
 
-/* const getBerto = async() =>{
-    try {
-        const response = await fetch("./quiz.json");
-        const search = await response.json();
-        const result = search;
-        return result;
-    } catch (error) {
-        console.error(error);
+        /* function validarRespuesta(){
+            const respuestaSeleccionada = document.getElementById("1")
+            const respuestaCorrecta = pregunta.correct;
+
+            if( respuestaSeleccionada === respuestaCorrecta){
+                alert("Correcto!");
+            }
+
+            else {
+                alert("Te has equivocado!");
+            }
+        }; */
+
+         function verificarRespuestaYAvanzar() {
+            
+        preguntaActual = (preguntaActual + 1) % preguntas.length;
+        mostrarPregunta();
     }
-};
+
+       /*  function verificarRespuestaYAvanzar() {
+
+            let respuestaSeleccionada = document.getElementById("uno")
+            let respuestaCorrecta = pregunta.correct;
+            console.log(respuestaCorrecta);
+
+            if( respuestaSeleccionada === respuestaCorrecta){
+                preguntaActual = (preguntaActual + 1) % preguntas.length;
+                mostrarPregunta();
+            }
+
+            else {
+                alert("Te has equivocado!");
+            }
+            
+           
+        } */
 
 
-const juego = async(pregunta) => {
-    const game = await getBerto(pregunta);
-    console.log('GAME', game);
-    const container = document.getElementById('container');
-    container.innerHTML = `<h1>${game.map((element) => element)
-    .join('')}</h1>`;
-}
- */
 
 
 
 
+{/* <label for="2">
 
+<li><input type="radio" id="2" name="options"/>${respuesta}</li>
+
+</label>
+
+<label for="3">
+
+<li><input type="radio" id="3" name="options"/>${respuesta}</li>
+
+</label>
+
+<label for="4">
+
+<li><input type="radio" id="4" name="options"/>${respuesta}</li>
+
+</label> */}
 
 
 
